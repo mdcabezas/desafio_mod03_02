131e7bac6fa1f624f66f5c99f1a82f72de1c3efa
Manuel Cabezas <manuelcabezas@gmail.com>
2022-12-03 16:57:08 -0400

    feat: add part two

---
+++ assets/js/parte2.js
@@ -0,0 +1,16 @@
+// Inicializa variables de elementos input
+const $inputjs = document.querySelector("#inputjs");
+const $inputnode = document.querySelector("#inputnode");
+const $inputreact = document.querySelector("#inputreact");
+const $displaytotal = document.querySelector("#displaytotal");
+
+function totalStickers() {
+    // Suma valores ingresados
+    const total = Number($inputjs.value) + Number($inputnode.value) + Number($inputreact.value);
+
+    if (total > 10) {
+        $displaytotal.innerHTML = "Llevas demasiados stickers";
+    } else {
+        $displaytotal.innerHTML = `Llevas <b>${total}</b> stickers`;
+    }
+}
